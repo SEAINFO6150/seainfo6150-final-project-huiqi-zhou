024a6d04583fdae98027db8d320d862062fb00e6
Ann0407 <zhouhq95@gmail.com>
2020-03-25 14:02:54 -0700

    add home page image and css

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Form from "../Form/Form";
-import Bar from "./Bar.jsx";
 import ProductInformation from "./ProductInformation.jsx";
+import style from "./Home.module.css";
 
 const Home = () => {
     return (
         <div>
-            <h1>H'Shop</h1>
-            <ProductInformation imageLink="/images/treefrog.jpg" imageAlt="tree frog" productName="aaa" price="$100"/>
-            <ProductInformation imageLink="/images/treefrog.jpg" imageAlt="tree frog" productName="aaa" price="$100"/>
-            <ProductInformation imageLink="/images/treefrog.jpg" imageAlt="tree frog" productName="aaa" price="$100"/>
-            <ProductInformation imageLink="/images/treefrog.jpg" imageAlt="tree frog" productName="aaa" price="$100"/>
-            <ProductInformation imageLink="/images/treefrog.jpg" imageAlt="tree frog" productName="aaa" price="$100"/>
-            <Form />
+            <h1 className={style.title}>H'Shop</h1>
+            <hr/>
+            <div className={style.grid}>
+            <ProductInformation imageLink="/images/blueblazer.jpg" imageAlt="blue blazer" productName="Blue Blazer" price="$100"/>
+            <ProductInformation imageLink="/images/flowerdress.jpg" imageAlt="flower dress" productName="Flower Dress" price="$100"/>
+            <ProductInformation imageLink="/images/whitedress.jpg" imageAlt="white dress" productName="White Dress" price="$100"/>
+            <ProductInformation imageLink="/images/bluejacket.jpg" imageAlt="blue jacket" productName="Blue Jacket" price="$100"/>
+            <ProductInformation imageLink="/images/begieblazer.jpg" imageAlt="begie blazer" productName="Begie Blazer" price="$100"/>
+            </div>
         </div>
     )
 }
--- src/Home/ProductInformation.jsx
+++ src/Home/ProductInformation.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Image.module.css'
 
 const ProductInformation = ({imageLink, imageAlt, productName, price}) => {
     return (
         <div>
-            <img src={imageLink} alt={imageAlt} />
+            <img className={styles.rectangle} src={imageLink} alt={imageAlt} />
             <h6>{productName}</h6>
             <h6>{price}</h6>
         </div>
