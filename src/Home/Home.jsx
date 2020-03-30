5d46038f74576d2860a9d8ec26988309f6477d90
Ann0407 <zhouhq95@gmail.com>
2020-03-30 16:39:41 -0700

    add css for pages

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -9,17 +9,18 @@ const Home = () => {
     return (
         <div>
             <h1 className={style.title}>H'Shop</h1>
+            <p>new clothing editor</p>
             <hr/>
             <div className={style.productgrid}>
-            <ProductInformation imageLink="/images/blueblazer.jpg" imageAlt="blue blazer" productName="Blue Blazer" price="$100"/>
-            <ProductInformation imageLink="/images/flowerdress.jpg" imageAlt="flower dress" productName="Flower Dress" price="$100"/>
-            <ProductInformation imageLink="/images/whitedress.jpg" imageAlt="white dress" productName="White Dress" price="$100"/>
-            <ProductInformation imageLink="/images/bluejacket.jpg" imageAlt="blue jacket" productName="Blue Jacket" price="$100"/>
-            <ProductInformation imageLink="/images/begieblazer.jpg" imageAlt="begie blazer" productName="Begie Blazer" price="$100"/>
-            <ProductInformation imageLink="/images/begieblazer.jpg" imageAlt="begie blazer" productName="Begie Blazer" price="$100"/>
-            <ProductInformation imageLink="/images/flowerdress.jpg" imageAlt="flower dress" productName="Flower Dress" price="$100"/>
-            <ProductInformation imageLink="/images/whitedress.jpg" imageAlt="white dress" productName="White Dress" price="$100"/>
-            <ProductInformation imageLink="/images/bluejacket.jpg" imageAlt="blue jacket" productName="Blue Jacket" price="$100"/>
+            <ProductInformation  imageLink="/images/blueblazer.jpg" imageAlt="blue blazer" productName="Blue Blazer" price="$100"/>
+            <ProductInformation  imageLink="/images/flowerdress.jpg" imageAlt="flower dress" productName="Flower Dress" price="$100"/>
+            <ProductInformation  imageLink="/images/whitedress.jpg" imageAlt="white dress" productName="White Dress" price="$100"/>
+            <ProductInformation  imageLink="/images/bluejacket.jpg" imageAlt="blue jacket" productName="Blue Jacket" price="$100"/>
+            <ProductInformation  imageLink="/images/begieblazer.jpg" imageAlt="begie blazer" productName="Begie Blazer" price="$100"/>
+            <ProductInformation  imageLink="/images/begieblazer.jpg" imageAlt="begie blazer" productName="Begie Blazer" price="$100"/>
+            <ProductInformation  imageLink="/images/flowerdress.jpg" imageAlt="flower dress" productName="Flower Dress" price="$100"/>
+            <ProductInformation  imageLink="/images/whitedress.jpg" imageAlt="white dress" productName="White Dress" price="$100"/>
+            <ProductInformation  imageLink="/images/bluejacket.jpg" imageAlt="blue jacket" productName="Blue Jacket" price="$100"/>
             </div>
             <hr/>
             <div className={style.infogrid}>
--- src/Home/ProductInformation.jsx
+++ src/Home/ProductInformation.jsx
@@ -5,8 +5,8 @@ const ProductInformation = ({imageLink, imageAlt, productName, price}) => {
     return (
         <div>
             <img className={styles.rectangle} src={imageLink} alt={imageAlt} />
-            <h6>{productName}</h6>
-            <h6>{price}</h6>
+            <h6 className={styles.text} >{productName}</h6>
+            <h6 className={styles.text}>{price}</h6>
         </div>
     )
 }
